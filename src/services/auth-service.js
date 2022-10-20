939ecda517d13ebc31c437b191d71487f11e6121
Larry <ddrippingcoffee@gmail.com>
2022-10-21 04:11:19 +0800

    刪除 iml 移除 path url /api 變更 axios 版本 修改檔名為連字號

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8085/richpwd/api/auth/'
+const API_URL = 'http://localhost:8085/richpwd/auth/'
 
 class AuthService {
     login (user) {
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 
-const API_URL = 'http://localhost:8085/richpwd/api/test/'
+const API_URL = 'http://localhost:8085/richpwd/test/'
 
 class UserService {
     getPublicContent () {
