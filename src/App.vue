bde964aac63ad17c20f6dec64c280c24ea987996
Larry <ddrippingcoffee@gmail.com>
2022-11-24 04:56:24 +0800

    App.vue 移除多餘 html tag

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,74 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a href="/" class="navbar-brand">Rich Pwd</a>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/home" class="nav-link">
-            <font-awesome-icon icon="home"/>
-            Home
-          </router-link>
-        </li>
-        <li v-if="currentUser" class="nav-item">
-          <router-link to="/cominfoPage" class="nav-link">
-            <font-awesome-icon icon="industry"/>
-            Cominfo
-          </router-link>
-        </li>
-        <li v-if="currentUser" class="nav-item">
-          <router-link to="/stentryPage" class="nav-link">
-            StEntryPage
-          </router-link>
-        </li>
-        <li v-if="currentUser" class="nav-item">
-          <router-link to="/entry" class="nav-link">Stock Entry</router-link>
-        </li>
-        <li v-if="currentUser" class="nav-item">
-          <router-link to="/entryOld" class="nav-link">Old Stock Entry</router-link>
-        </li>
-        <li v-if="showAdminBoard" class="nav-item">
-          <router-link to="/admin" class="nav-link">Admin Board</router-link>
-        </li>
-        <li v-if="showModeratorBoard" class="nav-item">
-          <router-link to="/mod" class="nav-link">Moderator Board</router-link>
-        </li>
-        <li v-if="currentUser" class="nav-item">
-          <router-link to="/user" class="nav-link">User</router-link>
-        </li>
-      </div>
-
-      <div v-if="!currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link">
-            <font-awesome-icon icon="user-plus"/>
-            Sign Up
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">
-            <font-awesome-icon icon="sign-in-alt"/>
-            Login
-          </router-link>
-        </li>
-      </div>
-
-      <div v-if="currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/profile" class="nav-link">
-            <font-awesome-icon icon="user"/>
-            {{ currentUser.username }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" @click.prevent="logOut">
-            <font-awesome-icon icon="sign-out-alt"/>
-            LogOut
-          </a>
-        </li>
-      </div>
-    </nav>
-    <br/>
-    <div class="container">
-      <router-view/>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a href="/" class="navbar-brand">Rich Pwd</a>
+    <div class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <router-link to="/home" class="nav-link">
+          <font-awesome-icon icon="home"/>
+          Home
+        </router-link>
+      </li>
+      <li v-if="currentUser" class="nav-item">
+        <router-link to="/cominfoPage" class="nav-link">
+          <font-awesome-icon icon="industry"/>
+          Cominfo
+        </router-link>
+      </li>
+      <li v-if="currentUser" class="nav-item">
+        <router-link to="/stentryPage" class="nav-link">
+          StEntryPage
+        </router-link>
+      </li>
+      <li v-if="currentUser" class="nav-item">
+        <router-link to="/entry" class="nav-link">Stock Entry</router-link>
+      </li>
+      <li v-if="currentUser" class="nav-item">
+        <router-link to="/entryOld" class="nav-link">Old Stock Entry</router-link>
+      </li>
+      <li v-if="showAdminBoard" class="nav-item">
+        <router-link to="/admin" class="nav-link">Admin Board</router-link>
+      </li>
+      <li v-if="showModeratorBoard" class="nav-item">
+        <router-link to="/mod" class="nav-link">Moderator Board</router-link>
+      </li>
+      <li v-if="currentUser" class="nav-item">
+        <router-link to="/user" class="nav-link">User</router-link>
+      </li>
     </div>
+
+    <div v-if="!currentUser" class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <router-link to="/register" class="nav-link">
+          <font-awesome-icon icon="user-plus"/>
+          Sign Up
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/login" class="nav-link">
+          <font-awesome-icon icon="sign-in-alt"/>
+          Login
+        </router-link>
+      </li>
+    </div>
+
+    <div v-if="currentUser" class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <router-link to="/profile" class="nav-link">
+          <font-awesome-icon icon="user"/>
+          {{ currentUser.username }}
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" @click.prevent="logOut">
+          <font-awesome-icon icon="sign-out-alt"/>
+          LogOut
+        </a>
+      </li>
+    </div>
+  </nav>
+  <div class="container">
+    <br/>
+    <router-view/>
   </div>
 </template>
 
